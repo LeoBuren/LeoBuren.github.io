905fb9067f996bf3c02d24a58dda06339b81bfd5
LeoBuren <leo.buren@hotmial.com>
2019-05-08 09:38:37 +0200

    added second section

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,5 @@
+import LazyLoadImages from './lazyload';
+
 /*$(document).on('click', 'a[href^="#"]', function() {
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top-48
@@ -34,12 +36,12 @@ const hideButtonEl = document.querySelector('.js-menu-hide');
 const sideNavEl = document.querySelector('aside');
 const sideNavContainerEl = document.querySelector('.side-nav-container');
 
-showButtonEl.addEventListener('click', () => sideNavEl.classList.add('side-nav--visible'));
-hideButtonEl.addEventListener('click', () => sideNavEl.classList.remove('side-nav--visible'));
-sideNavEl.addEventListener('click', e => e.target==sideNavContainerEl?null:sideNavEl.classList.remove('side-nav--visible'));
+showButtonEl.addEventListener('click', () => sideNavEl.classList.add('side-nav-visible'));
+hideButtonEl.addEventListener('click', () => sideNavEl.classList.remove('side-nav-visible'));
+sideNavEl.addEventListener('click', e => e.target==sideNavContainerEl?null:sideNavEl.classList.remove('side-nav-visible'));
 
 const headerEl = document.querySelector('.header-container');
-let headerOffset = headerEl.offsetTop;
+let headerOffset = headerEl.scrollHeight-44;
 
 window.addEventListener('scroll', () => window.pageYOffset >= headerOffset ? 
     headerEl.classList.add('sticky'):headerEl.classList.remove('sticky'), 
@@ -49,4 +51,7 @@ window.addEventListener('scroll', () => window.pageYOffset >= headerOffset ?
     }
 );
 
-document.addEventListener("DOMContentLoaded", () => sideNavContainerEl.style.display = 'flex');
+document.addEventListener("DOMContentLoaded", () => {
+    sideNavContainerEl.style.display = 'flex';
+    new LazyLoadImages();
+});
