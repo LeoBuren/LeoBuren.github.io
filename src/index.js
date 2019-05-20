9393e707a71ae9f645f8a8670e4cb700b15986a5
LeoBuren <leo.buren@hotmial.com>
2019-05-20 02:39:34 +0200

    square

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('offline-plugin/runtime').install();
 
 import './fonts/libre-baskerville-v5-latin-regular.woff';
 import './fonts/libre-baskerville-v5-latin-regular.woff2';
+import './images/Leo_Buren_CV.pdf';
 
 import './index.html';
 import './about.html';
