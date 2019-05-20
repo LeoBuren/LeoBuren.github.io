b05b2404c33a817a6d117fd399e3637e889ad9a7
LeoBuren <leo.buren@hotmial.com>
2019-05-21 01:11:04 +0200

    added text

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 require('offline-plugin/runtime').install();
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 import './images/Leo_Buren_CV.pdf';
 
 import './index.html';
