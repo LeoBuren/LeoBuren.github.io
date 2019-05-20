42333496c4d410ad4a9ca4090f5265932d95eb05
LeoBuren <leo.buren@hotmail.com>
2019-05-20 17:08:34 +0200

    fixed tabindex

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 require('offline-plugin/runtime').install();
-
-import './fonts/libre-baskerville-v5-latin-regular.woff';
-import './fonts/libre-baskerville-v5-latin-regular.woff2';
 import './images/Leo_Buren_CV.pdf';
 
 import './index.html';
